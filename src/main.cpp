8e0dc8b7deda3c0e0ffa4fc8e1164b3f8532ee6f
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-12 19:31:41 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer) {
+	void onPlay(CCObject* target)
+	   FLAlertLayer create(
+		"HAHA"
+		"You're trapped here FOREVER!!"
+		"OK"
+	   )->show();
+};
+
+	void onIconKit(CCObject* target) {
+	   FLAlertLayer create(
+		"HAHA"
+		"You're trapped here FOREVER!!"
+		"OK"
+	   )->show();
+};
+
+	void onMoreGames(CCObject* target) {
+	   FLAlertLayer create(
+		"HAHA"
+		"You're trapped here FOREVER!!"
+		"OK"
+	   )->show();
+};
+	void onEditor(CCObject* target) {
+	   FLAlertLayer create(
+		"HAHA"
+		"You're trapped here FOREVER!!"
+		"OK"
+	   )->show();
+};
+	void onProfile(CCObject* target) {
+	   FLAlertLayer create(
+		"HAHA"
+		"You're trapped here FOREVER!!"
+		"OK"
+	   )->show();
+};
+
