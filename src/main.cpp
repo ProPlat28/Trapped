0f2e52ffd3310be3f407d0a91b8052249d782ca9
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-13 10:08:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 
 using namespace geode::prelude;
 
+static void hideButtons(CCNode* node) {
+    if (!node)
+        return;
+
+    auto id = std::string(node->getID());
+
+    if (id == "dankmeme.globed2/main-menu-button" ||
+        id == "geode.loader/geode-button") {
+        node->setVisible(false);
+    }
+
+    for (auto child : CCArrayExt<CCNode*>(node->getChildren())) {
+        hideButtons(child);
+    }
+}
+
 class $modify(MenuLayer) {
 
     void showPopup() {
@@ -17,19 +33,11 @@ class $modify(MenuLayer) {
         showPopup();
     }
 
-    void onGarage(CCObject* target) {
+    void onArchievements(CCObject* target) {
         showPopup();
     }
-   
+
     void onCreator(CCObject* target) {
-        showPopup();
-    }
-
-    void onDaily(CCObject* target) {
-        showPopup();
-    }
-
-    void onOptions(CCObject* target) {
         showPopup();
     }
 
@@ -37,11 +45,23 @@ class $modify(MenuLayer) {
         showPopup();
     }
 
-    void onStats(CCObject* target) {
+    void onRobTop(CCObject* target) {
         showPopup();
     }
 
-    void onAchievements(CCObject* target) {
+    void onGarage(CCObject* target) {
+        showPopup();
+    }
+
+    void onOptions(CCObject* target) {
+        showPopup();
+    }
+
+    void onDaily(CCObject* target) {
+        showPopup();
+    }
+
+    void onStats(CCObject* target) {
         showPopup();
     }
 
@@ -49,7 +69,7 @@ class $modify(MenuLayer) {
         showPopup();
     }
 
-    void onQuit(CCObject* target) {
+    void willClose(CCObject* target) {
         showPopup();
     }
 
@@ -57,47 +77,6 @@ class $modify(MenuLayer) {
         showPopup();
     }
 
-    void onRobTop(CCObject* target) {
-        showPopup();
-    }
-
-    void onYouTube(CCObject* target) {
-        showPopup();
-    }
-
-    void onTwitch(CCObject* target) {
-        showPopup();
-    }
-
-    void onDiscord(CCObject* target) {
-        showPopup();
-    }
-
-    void onTwitter(CCObject* target) {
-        showPopup();
-    }
-
-    void onFacebook(CCObject* target) {
-        showPopup();
-    }
-
-static void hideButtons(cocos2d::CCNode* node) {
-    if (!node)
-        return;
-
-    auto id = std::string(node->getID());
-
-    if (id == "dankmeme.globed2/mein-menu-button" ||
-        id == "geode.loader/geode-button") {
-        node->setVisible(false);
-    }
-
-    for (auto child : CCArrayExt<cocos2d::CCNode*>(node->getChildren())) {
-        hideButtons(child);
-    }
-}
-
-class $modify(MenuLayer) {
     bool init() {
         if (!MenuLayer::init())
             return false;
