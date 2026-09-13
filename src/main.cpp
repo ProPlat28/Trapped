38706d4e3e4c5d220f1cc5f2404f69dcbd9e5e30
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-13 09:32:54 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 
@@ -17,11 +18,11 @@ class $modify(MenuLayer) {
         showPopup();
     }
 
-    void onLevelEditor(CCObject* target) {
+    void onGarage(CCObject* target) {
         showPopup();
     }
-
-    void onGarage(CCObject* target) {
+   
+    void onCreator(CCObject* target) {
         showPopup();
     }
 
@@ -29,11 +30,76 @@ class $modify(MenuLayer) {
         showPopup();
     }
 
+    void onOptions(CCObject* target) {
+        showPopup();
+    }
+
+    void onNewgrounds(CCObject* target) {
+        showPopup();
+    }
+
+    void onArchievements(CCObject* target) {
+        showPopup();
+    }
+
+    void onStats(CCObject* target) {
+        showPopup();
+    }
+
     void onMoreGames(CCObject* target) {
+        showPopup();
+    }
+
+    void willClose(CCObject* target) {
         showPopup();
     }
 
     void onMyProfile(CCObject* target) {
         showPopup();
+    }
+
+    void onRobTop(CCObject* target) {
+        showPopup();
+    }
+
+    void onYoutube(CCObject* target) {
+        showPopup();
+    }
+
+    void onTwitch(CCObject* target) {
+        showPopup();
+    }
+
+    void onDiscord(CCObject* target) {
+        showPopup();
+    }
+
+    void onTwitter(CCObject* target) {
+        showPopup();
+    }
+
+    void onFacebook(CCObject* target) {
+        showPopup();
+    }
+
+    bool init() {
+        if (!MenuLayer::init())
+            return false;
+
+        for (auto child : CCArrayExt<CCNode*>(this->getChildren())) {
+            if (!child)
+                continue;
+
+            auto id = child->getID();
+            if (id && std::string(id).find("geode") != std::string::npos) {
+                child->setVisible(false);
+            }
+        }
+
+        if (auto globed = this->getChildByID("dankmeme.globed2")) {
+            globed->setVisible(false);
+        }
+
+        return true;
     }
 };
